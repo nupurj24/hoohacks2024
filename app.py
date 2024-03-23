2d9fb2d64a3466039150a6b203931b1fac290df5
Amanda Xiao <amanda64@email.unc.edu>
2024-03-23 18:44:38 -0400

    Set up Google Map and Place API for Ecoeater

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ecoeater')
+def ecoeater():
+    return render_template('ecoeater.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
