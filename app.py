ca49a4124192642991864e06330909a36c935923
Nupur Joshi <nupurj24@email.unc.edu>
2024-03-23 20:52:34 -0400

    Need to make response formatted correctly to return to user.

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask, render_template, request 
+from openai import OpenAI
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+def get_completion(prompt,):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        messages=messages,
+        model="gpt-3.5-turbo"
+    )
+    return response
+
+@app.route("/assistant")
+def home():    
+    return render_template("assistant.html")
+
+@app.route("/assistant/get")
+def get_bot_response():    
+    userText = request.args.get('msg')  
+    completion = get_completion(userText)
+    bot_response = completion['choices'][0]['message']['content']
+    return bot_response
 
 if __name__ == '__main__':
     app.run(debug=True)
